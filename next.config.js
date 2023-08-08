601c454da7237a5359c643e4e08b837435e0151b
tmilost <tmilos94@gmail.com>
2023-08-08 21:24:57 +0200

    add cache controll

--- next.config.js
+++ next.config.js
@@ -2,3 +2,20 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    async headers() {
+      return [
+        {
+            source: '/:all*(svg|jpg|png)',
+            locale: false,
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=9999999999, must-revalidate',
+              }
+            ],
+          },
+      ]
+    },
+  }
