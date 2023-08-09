6f4aed3ba7c73ded09adecc8b2cbe92e5be44e94
tmilost <tmilos94@gmail.com>
2023-08-09 02:02:03 +0200

    revet config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: './',
-  };
+  output: 'export',
+};
 
 module.exports = nextConfig;
 
