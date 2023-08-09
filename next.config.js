2d5834b7017caffdb46edae690523961dd0ac1eb
tmilost <tmilos94@gmail.com>
2023-08-09 02:27:57 +0200

    next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    output: 'export'
+  }
 
 module.exports = nextConfig;
 
